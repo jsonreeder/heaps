6f187e518991b92f555faf21dd10bce212c02c26
Jason Reeder <info@jasonreeder.com>
2017-04-17 10:19:18 -0700

    Get right return value

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,5 +2,15 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    sorted = []
+    descending = Proc.new { |el1, el2| -1 * (el1 <=> el2) }
+    heap = BinaryMinHeap.new(&descending)
+    self.each { |el| heap.push(el) }
+
+    until sorted.length == self.length
+      sorted << heap.extract
+    end
+
+    sorted
   end
 end
