a92a5ff85e07b9a7b1e61227b6f6fbd820e7995a
Jason Reeder <info@jasonreeder.com>
2017-04-16 22:26:04 -0700

    Add heapify down

--- lib/heap.rb
+++ lib/heap.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class BinaryMinHeap
   def initialize(&prc)
     @store = []
@@ -30,6 +32,20 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    child_idxs = BinaryMinHeap.child_indices(len, parent_idx)
+    children = child_idxs.map { |el| array[el] }
+    parent = array[parent_idx]
+
+    while children.any? { |child| child < parent }
+      first_child_idx = child_idxs[0]
+      array[parent_idx], array[first_child_idx] = array[first_child_idx], array[parent_idx]
+      parent_idx = first_child_idx
+      child_idxs = BinaryMinHeap.child_indices(len, parent_idx)
+      children = child_idxs.map { |el| array[el] }
+      parent = array[parent_idx]
+    end
+
+    array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
